f7f75be7ce015746cd32af8fc56d7a3e3efb03da
buptssemx <buptssemx@163.com>
2021-11-11 14:12:36 +0800

    添加断言异常及统一日志

--- service-core/src/main/java/com/max/srb/core/pojo/entity/IntegralGrade.java
+++ service-core/src/main/java/com/max/srb/core/pojo/entity/IntegralGrade.java
@@ -49,7 +49,7 @@ public class IntegralGrade implements Serializable {
     @ApiModelProperty(value = "逻辑删除(1:已删除，0:未删除)")
 //    @TableField("is_deleted")
     @TableLogic
-    private Boolean deleted;
+    private Boolean isDeleted;
 
 
 }
